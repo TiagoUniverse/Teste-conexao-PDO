c18a2d79b0fbfbfb9e2adae8dd8fa741487943f3
TiagoUniverse <tiagocesar68@gmail.com>
2023-01-26 14:59:53 -0300

    estudando conexões e funções

--- conectar.php
+++ conectar.php
@@ -7,3 +7,14 @@ try{
 } catch(Exception $e){
     die (print_r ($e->getMessage()));
 }
+
+$stmt = $db->prepare('Select * from Usuario;');
+$stmt->execute();
+$result = $stmt->fetch(PDO::FETCH_OBJ);
+
+// $stmt->debugDumpParams();
+
+// echo $stmt->columnCount();
+
+
+print_r($result);
