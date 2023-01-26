042be28725b3a6a79cd32da637a3fe8a363a0047
TiagoUniverse <tiagocesar68@gmail.com>
2023-01-26 14:22:20 -0300

    Consegui conectar utilizando o framework

---
+++ conectar.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "SQLSERVER";
+try{
+    $db = new PDO ("sqlsrv:server=$servername ; Database=Placement" , "tiagolopes" , "minhaSenha");
+    echo "Conectado com sucesso!";
+    $db->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+} catch(Exception $e){
+    die (print_r ($e->getMessage()));
+}
