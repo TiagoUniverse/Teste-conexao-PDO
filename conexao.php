1c48a3715272d89364faa7f030113c6d7c241ed9
TiagoUniverse <tiagocesar68@gmail.com>
2023-01-26 15:48:44 -0300

    Arquivo de conexão configurado

--- conexao.php
+++ conexao.php
@@ -1,8 +1,11 @@
 <?php
 $servername = "SQLSERVER";
+$dbname = "Placement";
+$username = "tiagolopes";
+$pwd = "gti2022";
 try{
-    $db = new PDO ("sqlsrv:server=$servername ; Database=Placement" , "tiagolopes" , "minhaSenha");
-    // echo "Conectado com sucesso!";
+    $db = new PDO ("sqlsrv:server=$servername ; Database=$dbname" , "$username" , "$pwd");
+    echo "Conectado com sucesso!";
     $db->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
 } catch(Exception $e){
     die (print_r ($e->getMessage()));
