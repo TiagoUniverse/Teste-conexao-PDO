6c2626cc25c9985d70e27809e7dc4ba00b586bc8
TiagoUniverse <tiagocesar68@gmail.com>
2023-01-26 15:54:00 -0300

    Exercício 01 - exec

--- conexao.php
+++ conexao.php
@@ -5,7 +5,7 @@ $username = "tiagolopes";
 $pwd = "gti2022";
 try{
     $db = new PDO ("sqlsrv:server=$servername ; Database=$dbname" , "$username" , "$pwd");
-    echo "Conectado com sucesso!";
+    // echo "Conectado com sucesso!";
     $db->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
 } catch(Exception $e){
     die (print_r ($e->getMessage()));
---
+++ exercicios/aula01_exec.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "../conexao.php";
+
+/**
+ * PDO::exec -- Executa uma instrução SQL e retorna o número de linhas afetadas
+ */
+
+ try{
+
+ } catch(PDOException $err){
+    echo $erro->getMessage();
+ }
